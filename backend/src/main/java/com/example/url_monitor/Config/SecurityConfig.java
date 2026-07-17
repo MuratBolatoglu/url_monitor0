6e075567e3e94fa4fcd71ab78e65c510d9754346
Murat <murat.bolatoglu@gmail.com>
2026-07-17 03:50:40 +0300

    xd

--- backend/src/main/java/com/example/url_monitor/Config/SecurityConfig.java
+++ backend/src/main/java/com/example/url_monitor/Config/SecurityConfig.java
@@ -4,12 +4,16 @@ import com.example.url_monitor.Security.JwtAuthenticationFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-
+import java.util.List;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 @Configuration
 @RequiredArgsConstructor
 public class SecurityConfig {
@@ -19,6 +23,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+                .cors(Customizer.withDefaults())
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth.requestMatchers("/auth/**","/users").permitAll().anyRequest().authenticated())
                 .httpBasic(httpBasic -> httpBasic.disable())
@@ -31,5 +36,18 @@ public class SecurityConfig {
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+
+        configuration.setAllowedOrigins(List.of("http://localhost:5173"));
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(List.of("*"));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+
+        return source;
     }
 }
--- frontend/src/pages/login.tsx
+++ frontend/src/pages/login.tsx
@@ -1,11 +1,24 @@
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { useState } from "react";
+import api from "@/services/api";
 
 function Login() {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+
+    function handleLogin() {
+        try {
+            const response = api.post("auth/login", { password,email });
+            console.log("Login successful");
+            console.log(response);
+        }catch (error) {
+            console.error(error);
+        }
+        
+    }
+
     return (
         <div>
             <Card>
@@ -17,10 +30,14 @@ function Login() {
 
                 <CardContent>
                     <Input value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Email"/>
-                    <Input value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Password"/>  
+                    <Input value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Password" type="password"/>  
                 </CardContent>
 
-                
+                <CardFooter>
+                    <Button type="button" onClick={()=>handleLogin()}>
+                        Login
+                    </Button>
+                </CardFooter>
             </Card>
         </div>
         
@@ -29,3 +46,4 @@ function Login() {
 }
 
 export default Login;
+
