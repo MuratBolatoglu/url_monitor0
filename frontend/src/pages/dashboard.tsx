e88c74a964a7cf3ee112fb9b1c332079f31cbc9f
Murat <murat.bolatoglu@gmail.com>
2026-07-20 19:45:35 +0300

    save2

--- frontend/src/pages/dashboard.tsx
+++ frontend/src/pages/dashboard.tsx
@@ -1,29 +1,27 @@
-import { Button } from "@/components/ui/button";
-import { useNavigate } from "react-router-dom";
-import MonitorCard from "@/components/MonitorCard";
-import type { Monitor } from "@/types/Monitor";
 import { useEffect, useState } from "react";
-import api from "@/services/api";
+import { useNavigate } from "react-router-dom";
+import { Button } from "@/components/ui/button";
 import AddMonitorDialog from "@/components/AddMonitorDialog";
+import MonitorCard from "@/components/MonitorCard";
+import api from "@/services/api";
+import type { Monitor } from "@/types/Monitor";
 
 function Dashboard() {
     const navigate = useNavigate();
-    const [monitors,setMonitors] = useState<Monitor[]>([])
-        async function fetchMonitors(){
-            try {
-                const response = await api.get("/monitors")
-                setMonitors(response.data);
-            } catch (error) {
-                console.log(error)
-            }
+    const [monitors, setMonitors] = useState<Monitor[]>([]);
 
+    async function fetchMonitors() {
+        try {
+            const response = await api.get("/monitors");
+            setMonitors(response.data);
+        } catch (error) {
+            console.error(error);
         }
+    }
 
-    useEffect(()=> {
+    useEffect(() => {
         fetchMonitors();
-    },[]);
-
-
+    }, []);
 
     function handleLogout() {
         localStorage.removeItem("token");
@@ -31,20 +29,18 @@ function Dashboard() {
     }
 
     return (
-    <div className="min-h-screen p-8">
-        <div className="flex justify-between items-center">
-            <h1 className="text-3xl font-bold">Dashboard</h1>
-            <div className="flex items-center gap-2">
-                <AddMonitorDialog onMonitorCreated={fetchMonitors}/>
-                <Button onClick={handleLogout}>Logout</Button>
+        <div className="min-h-screen p-8">
+            <div className="flex items-center justify-between">
+                <h1 className="text-3xl font-bold">Dashboard</h1>
+                <div className="flex items-center gap-2">
+                    <AddMonitorDialog onMonitorCreated={fetchMonitors} />
+                    <Button onClick={handleLogout}>Logout</Button>
+                </div>
+            </div>
+            <div className="mt-8">
+                {monitors.map((monitor) => <MonitorCard key={monitor.id_var} {...monitor} />)}
             </div>
         </div>
-        <div className="mt-8">
-            {monitors.map((monitor) =>
-                <MonitorCard {...monitor} key={monitor.id_var}/>
-            )}
-        </div>
-    </div>
     );
 }
 
