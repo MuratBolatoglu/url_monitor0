e3215ae5edd57d7e9f525ae25546466f89b1cf40
Murat <murat.bolatoglu@gmail.com>
2026-07-24 13:26:59 +0300

    zbap ivj

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,6 @@
 import { BrowserRouter, Routes, Route ,Navigate} from "react-router-dom";
 import ProtectedRoute from "./components/ProtectedRoute";
 import Login from "./pages/login";
-import Register from "./pages/register";
 import Dashboard from "./pages/dashboard";
 
 function App() {
@@ -11,7 +10,6 @@ function App() {
                 <Route path="/login" element={<Login />} />
                 <Route path="*" element={<Navigate to="/login" />} />
                 <Route path="/" element={<Navigate to="/login" />} />
-                <Route path="/register" element={<Register/>} />
                 <Route path="/dashboard" element={<ProtectedRoute><Dashboard/></ProtectedRoute>} />
                 
             </Routes>
